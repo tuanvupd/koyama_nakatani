e5c2a0ca4f7657327ded6f0c8f6cc3457baeac77
Vu Nguyen <tuanvu.pd@gmail.com>
2025-10-23 07:13:33 +0700

    updat text legal

--- languages/fr/legal-information.php
+++ languages/fr/legal-information.php
@@ -6,8 +6,8 @@ return [
         [
             'title'   => '1. Présentation du site internet.',
             'content' => [
-                'Propriétaire : JMS SARL, Capital social de 10 000,00 € Numéro d’inscription au registre du commerce et des sociétés : 800 333 403 00015, sis 27 RUE PIERRE LEROUX 75007 PARIS – Numéro de TVA : FR58800333403 (ci-après dénommées JMS)',
-                'Responsable publication : JMS – Numéro de téléphone 01 47 34 94 14.',
+                'Propriétaire : JMS SARL, Capital social de 10 000,00 € Numéro d\'inscription au registre du commerce et des sociétés : 800 333 403 00015, sis 27 RUE PIERRE LEROUX 75007 PARIS – Numéro de TVA : FR58800333403 (ci-après dénommées JMS)',
+                'Résponsable publication : JMS – Numéro de téléphone 01 47 34 94 14.',
                 '<span class="space"> </span>',
                 'Webmaster : Nakatani Restaurant',
                 'info@restaurant-nakatani.com',
@@ -24,12 +24,12 @@ return [
         ],
 
         [
-            'title' => '2. Conditions générales d’utilisation du site et des services proposés.',
+            'title' => '2. Conditions générales d\'utilisation du site et des services proposés.',
             'content' => [
                 'Le Site constitue une œuvre de l\'esprit protégée par les dispositions du Code de la Propriété Intellectuelle et des Réglementations Internationales applicables. La tiers personne ne peut en aucune manière réutiliser ou exploiter pour son propre compte tout ou partie des éléments ou travaux du Site.',
-                'Les présentes Conditions Générales d\'Utilisation (ci-après dénommées CGU) ont pour objet de définir les modalités et conditions dans lesquelles, JMS met à la disposition des utilisateurs (ci-après dénommé les Utilisateurs) du site www.restaurant-nakatani.com, (ci-après dénommé le Site) les services associés et la manière par laquelle les Utilisateurs accèdent au Site et utilisent ses services. Toute connexion au Site est subordonnée au respect des CGU. L\'Utilisateur reconnaît à ce titre que le simple accès au Site implique l\'acceptation de l\'ensemble des conditions décrites ci-après.',
+                'Les présentes Conditions Générales d\'Utilisation (ci-après dénommées CGU) ont pour objet de définir les modalités et conditions dans lesquelles, JMS met à la disposition des utilisateurs (ci-après dénommé les Utilisateurs) du site www.restaurant-nakatani.com, (ci-après dénommé le Site) les services associés et la manière par laquelle les Utilisateurs accèdent au Site et utilisent ses services.Toute connexion au Site est subordonnée au respect des CGU. L\'Utilisateur reconnaît à ce titre que le simple accès au Site implique l\'acceptation de l\'ensemble des conditions décrites ci-après.',
                 'L\'Utilisateur du Site reconnaît avoir vérifié que la configuration informatique qu\'il utilise ne contient aucun virus et qu\'elle est en parfait état de fonctionnement.',
-                'Il est rappelé que le secret des correspondances n\'est pas garanti sur le réseau Internet et qu\'il appartient à chaque Utilisateur d\'Internet de prendre toutes les précautions appropriées de façon à protéger ses propres données et/ou logiciels de la contamination d\'éventuels virus circulant sur Internet. JMS ne saurait être tenue pour responsable d\'éventuels dommages, directs ou indirects, pouvant découler de l\'accès, de l\'utilisation ou du téléchargement de contenu via le Site, notamment, un dommage ou virus qui pourrait infecter l\'ordinateur de l\'Utilisateur ou tout autre matériel.',
+                'Il est rappelé que le secret des correspondances n\'est pas garanti sur le réseau Internet et qu\'il appartient à chaque Utilisateur d\'Internet de prendre toutes les précautions appropriées de façon à protéger ses propres données et/ou logiciels de la contamination d\'éventuels virus circulant sur Internet. JMS ne saurait être tenue pour responsable d\'éventuels dommages, directs ou indirects, pouvant découler de l\'accès, de l\'utilisation ou de téléchargement de contenu via le Site, notamment, un dommage ou virus qui pourrait infecter l\'ordinateur de l\'Utilisateur ou tout autre matériel.',
                 'JMS s\'efforce de permettre l\'accès au Site 24 heures sur 24, 7 jours sur 7, sauf en cas de force majeure ou d\'un événement hors de son contrôle, et sous réserve des éventuelles pannes et interventions de maintenance nécessaires au bon fonctionnement du Site et des services.',
                 'Par conséquent, JMS ne peut garantir une disponibilité du Site et/ou des services, une fiabilité des transmissions et des performances en termes de temps de réponse ou de qualité. Il n\'est prévu aucune assistance technique vis-à-vis de l\'Utilisateur que ce soit par des moyens électronique ou téléphonique.',
                 'La responsabilité de JMS ne saurait être engagée en cas d\'impossibilité d\'accès à ce Site et/ou d\'utilisation des services. Par ailleurs, JMS peut être amenée à interrompre le Site ou une partie des services, à tout moment sans préavis, le tout sans droit à indemnités. L\'Utilisateur reconnaît et accepte que JMS ne soit pas responsable des interruptions, et des conséquences qui peuvent en découler pour l\'Utilisateur ou tout tiers.'
@@ -46,8 +46,17 @@ return [
         [
             'title' => '4. Propriété intellectuelle et contrefaçons.',
             'content' => [
-                'Tout le contenu présent sur le site www.restaurant-nakatani.com, incluant, de façon non limitative, les graphismes, images, textes, vidéos, animations, sons, logos, gifs et icônes ainsi que leur mise en forme sont la propriété exclusive de JMS à l’exception des marques, logos ou contenus appartenant à d’autres sociétés partenaires ou auteurs.',
-                'Toute reproduction, distribution, modification, adaptation, retransmission ou publication, même partielle, de ces différents éléments est strictement interdite sans l’accord exprès par écrit de JMS. Cette représentation ou reproduction, par quelque procédé que ce soit, constitue une contrefaçon sanctionnée par les articles L.335-2 et suivants du Code de la propriété intellectuelle. Le non-respect de cette interdiction constitue une contrefaçon pouvant engager la responsabilité civile et pénale du contrefacteur. En outre, les propriétaires des Contenus copiés pourraient intenter une action en justice à votre encontre.'
+                'JMS est propriétaire des droits de propriété intellectuelle et détient les droits d\'usage sur tous les éléments accessibles sur le site internet',
+                'La structure générale du Site et son contenu sont la propriété exclusive de JMS, et font l\'objet d\'une protection légale au titre de la propriété intellectuelle.',
+                'Toute exploitation non autorisée du site ou de l\'un quelconque des éléments qu\'il contient sera considérée comme constitutive d\'une contrefaçon et poursuivie conformément aux dispositions des articles L.335-2 et suivants du Code de Propriété Intellectuelle.',
+                'Toute réutilisation ou extraction totale ou partielle, des bases de données du Site par une personne physique ou morale, par quelque procédé que ce soit, est prohibée et constituerait un acte de contrefaçon sanctionné par les articles L. 343-1 et suivants du Code de la propriété intellectuelle.'
+            ]
+        ],
+
+        [
+            'title' => '5. Gestion des données personnelles.',
+            'content' => [
+                'Conformément à la loi Informatique et Libertés n°78-17 du 6 janvier 1978 modifiée et au Règlement Général sur la Protection des Données à caractère personnel (ci-après « RGPD ») n° 2016/679/UE du 27 avril 2016, JMS, en qualité de responsable de traitement, garantit le respect de la vie privée des Utilisateurs du Site.'
             ]
         ],
     ],
